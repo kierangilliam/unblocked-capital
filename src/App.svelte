2505d7d17f6686833cc6626d531fb4aa4d1fae57
Kieran Gill <kieran.gill1997@gmail.com>
2021-02-19 17:57:34 -0800

    basic notion

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,61 @@
-<script lang="ts">
-  import { Router } from "@roxi/routify";
-  import { routes } from "../.routify/routes";
+<script lang='ts'>
+    import { Router } from '@roxi/routify'
+    import { routes } from '../.routify/routes'
+    import { Theme } from './components'
 </script>
 
-<style  global>
-  @import "../assets/global.css";
-</style>
+<Theme>
+    <div class='wrapper'>
+    <div class='title'>
+        <h4 on:click={() => window.location.href = '/'}>Unblocked Captial</h4>
+        <h4>Learning to code for algorithmic trading from scratch.</h4>
+    </div>
+      <div class='container'>
+          <Router {routes} />
+      </div>
+    </div>
+</Theme>
 
-<Router {routes} />
+<style>
+    @import '../assets/global.css';
+
+    :global(h1, h2, h3, .page-link) {
+        margin-bottom: var(--s-4) !important;
+    }
+    :global(p) {
+        margin-bottom: var(--s-3);
+        line-height: var(--s-8);
+    }
+    :global(.page-link) {
+        font-weight: bold;
+        padding: var(--s-2) var(--s-3);
+        border: 3px solid var(--darkBlue);
+        box-shadow: var(--level-1);
+        cursor: pointer;
+    }
+    :global(.page-link:hover) {
+        box-shadow: var(--level-2);
+    }
+     
+    .title {
+        margin-top: var(--s-6) !important;
+        margin-bottom: var(--s-8) !important;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        width: 60vw;
+        min-width: 400px;
+        border-bottom: 2px solid var(--darkGray);
+    }
+
+    .wrapper {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .container {
+        max-width: 40rem;
+    }
+</style>
--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,8 +1,26 @@
-<script>
-    import RoutifyIntro from './example/_components/RoutifyIntro.svelte'
-    import { metatags } from '@roxi/routify'
-    metatags.title = 'My Routify app'
-    metatags.description = 'Description coming soon...'
+<script lang='ts'>
+    import { fetchNotion } from '@leveluptuts/svelte-notion';
+    import { Notion } from '../components'
+    import { metatags, goto } from '@roxi/routify'
+
+    const notionData = (async () => {
+        const pageId = '2a42fe43d6cf4b5db39ba0ed6dd1be0d'
+        return await fetchNotion({ id: pageId, context: this })
+    })()
+
+    const gotoPage = (id: string) => () => {
+        $goto(`./post/${id}`)
+    }
+
+    metatags.title = 'Unblocked Captial'
+    metatags.description = 'Learn to code for algorithmic trading'
 </script>
 
-<RoutifyIntro />
+
+{#await notionData}
+    <p>...waiting</p>
+{:then blocks}
+    <Notion {blocks} {gotoPage} />
+{:catch error}
+    <p>An error occurred!</p>
+{/await}
